05a1c5fc8d78f7f246e3ed59663fea866fc4f518
Yuds <qunsyy@gmail.com>
2019-03-06 14:51:59 +0800

    Log In Test Case Added.

---
+++ Scripts/Log In/Script1551854961553.groovy
@@ -0,0 +1,28 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://pmrsqa.eschost2.com/')
+
+WebUI.delay(2)
+
+WebUI.setText(findTestObject('Object Repository/Log In/Page_PMRS Web Application/input_Username_mat-input-0'), 'test123')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Log In/Page_PMRS Web Application/input_Username_mat-input-1'), 
+    'qm7rzd0g/gseR/HEX70nNg==')
+
+WebUI.click(findTestObject('Object Repository/Log In/Page_PMRS Web Application/button_Login'))
+
